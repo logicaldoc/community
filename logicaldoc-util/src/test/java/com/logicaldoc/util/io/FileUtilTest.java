7f439b55c6767d679c6f6db3890890eaacb499b3
gasparez15 <a.gasparini@logicaldoc.com>
2022-10-17 15:19:50 +0200

    Added test for Exec

--- logicaldoc-util/src/test/java/com/logicaldoc/util/io/FileUtilTest.java
+++ logicaldoc-util/src/test/java/com/logicaldoc/util/io/FileUtilTest.java
@@ -2,22 +2,10 @@ package com.logicaldoc.util.io;
 
 import java.io.IOException;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FileUtilTest {
-
-	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	@After
-	public void tearDown() throws Exception {
-
-	}
 
 	@Test
 	public void testMatch() throws IOException {
