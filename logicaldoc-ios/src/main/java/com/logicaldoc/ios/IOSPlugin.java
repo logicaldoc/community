7bd4fbaa7d7ab8e9fde238c9d7210bdd126a7261
car031 <m.meschieri@logicaldoc.com>
2023-02-20 15:33:15 +0100

    Added module for ios App

---
+++ logicaldoc-ios/src/main/java/com/logicaldoc/ios/IOSPlugin.java
@@ -0,0 +1,16 @@
+package com.logicaldoc.ios;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.logicaldoc.util.plugin.LogicalDOCPlugin;
+
+/**
+ * Entry-point for the iOS Mobile plug-in
+ * 
+ * @author Marco Meschieri - LogicalDOC
+ * @since 8.8.4
+ */
+public class IOSPlugin extends LogicalDOCPlugin {
+	protected static Logger log = LoggerFactory.getLogger(IOSPlugin.class);
+}
