c147f6b418acf816dadc8c5ba73e64ccadafcd99
car031 <m.meschieri@logicaldoc.com>
2023-03-22 15:19:00 +0100

    removed system outs

--- logicaldoc-webapp/src/main/java/com/logicaldoc/web/service/TemplateServiceImpl.java
+++ logicaldoc-webapp/src/main/java/com/logicaldoc/web/service/TemplateServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
