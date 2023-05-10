38ed349c6b252d2b711f275c39f87c54853521cf
car031 <m.meschieri@logicaldoc.com>
2023-05-10 15:28:36 +0200

    Fixed extended attributes axtraction in CMIS

--- logicaldoc-cmis/src/main/java/com/logicaldoc/cmis/LDRepository.java
+++ logicaldoc-cmis/src/main/java/com/logicaldoc/cmis/LDRepository.java
@@ -1860,7 +1860,7 @@ public class LDRepository {
 
 		Template template = getTemplate(doc);
 
-		if (template == null || !(doc instanceof Hit)) {
+		if (template == null) {
 			addPropertyString(result, typeId, filter, TypeManager.PROP_TEMPLATE, null);
 			return;
 		}
