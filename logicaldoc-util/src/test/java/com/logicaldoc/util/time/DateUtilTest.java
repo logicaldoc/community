8c605d96761ec7529cf870a80b6a18bd7f388436
car031 <m.meschieri@logicaldoc.com>
2024-04-01 08:17:27 +0200

    fixed date format test time zone.

--- logicaldoc-util/src/test/java/com/logicaldoc/util/time/DateUtilTest.java
+++ logicaldoc-util/src/test/java/com/logicaldoc/util/time/DateUtilTest.java
@@ -8,36 +8,43 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class DateUtilTest extends TestCase {
-	
+
 	@Test
 	public void testFormat() throws ParseException {
-		Calendar calendar=Calendar.getInstance(Locale.US);
-		calendar.set(Calendar.YEAR, 1978);
-		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+		Calendar calendar = Calendar.getInstance(Locale.getDefault());
+		calendar.set(Calendar.YEAR, 1992);
+		calendar.set(Calendar.MONTH, Calendar.getInstance().get(Calendar.MONTH));
 		calendar.set(Calendar.DATE, 15);
 		calendar.set(Calendar.HOUR_OF_DAY, 23);
 		calendar.set(Calendar.MINUTE, 50);
 		calendar.set(Calendar.SECOND, 13);
 		calendar.set(Calendar.MILLISECOND, 27);
-		
+
 		DateFormat timeZoneFormat = new SimpleDateFormat("Z");
-		
-		assertEquals("1978-02-15 23:50:13 "+timeZoneFormat.format(new Date()), DateUtil.format(calendar.getTime()));
-	
-		assertEquals("1978-02-15 23:50:13.027 "+timeZoneFormat.format(new Date()), DateUtil.formatWithMillis(calendar.getTime()));	
+
+		assertEquals(
+				"1992-" + StringUtils.leftPad(Integer.toString(Calendar.getInstance().get(Calendar.MONTH) + 1), 2, '0')
+						+ "-15 23:50:13 " + timeZoneFormat.format(new Date()),
+				DateUtil.format(calendar.getTime()));
+
+		assertEquals(
+				"1992-" + StringUtils.leftPad(Integer.toString(Calendar.getInstance().get(Calendar.MONTH) + 1), 2, '0')
+						+ "-15 23:50:13.027 " + timeZoneFormat.format(new Date()),
+				DateUtil.formatWithMillis(calendar.getTime()));
 	}
-	
+
 	@Test
 	public void testParse() throws ParseException {
-		Date date=DateUtil.parse("1978-02-15 12:50:13.000 +0000");
+		Date date = DateUtil.parse("1978-02-15 12:50:13.000 +0000");
 		assertNotNull(date);
-		
-		Calendar calendar=Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+
+		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		calendar.set(Calendar.YEAR, 1978);
 		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
 		calendar.set(Calendar.DATE, 15);
@@ -47,13 +54,13 @@ public class DateUtilTest extends TestCase {
 		calendar.set(Calendar.MILLISECOND, 0);
 
 		assertEquals(calendar.getTime(), date);
-		
+
 		assertNull(DateUtil.parse("pippo"));
 	}
-	
+
 	@Test
 	public void testTruncateToDay() throws ParseException {
-		Calendar calendar=Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		calendar.set(Calendar.YEAR, 1978);
 		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
 		calendar.set(Calendar.DATE, 15);
