7911f22121f7172314084fb2164a5c678364549e
car031 <m.meschieri@logicaldoc.com>
2024-04-04 16:46:09 +0200

    Fixed locale detection

--- logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIValue.java
+++ logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIValue.java
@@ -41,4 +41,34 @@ public class GUIValue implements Serializable {
 	public void setValue(String value) {
 		this.value = value;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GUIValue other = (GUIValue) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return code.toString();
+	}
 }
