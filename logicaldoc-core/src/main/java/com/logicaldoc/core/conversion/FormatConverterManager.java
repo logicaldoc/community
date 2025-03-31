3c7080864b9514a5dd8e1a2cc49a0434f29d41c2
car031 <m.meschieri@logicaldoc.com>
2025-03-31 07:48:37 +0200

    Fixed format conmverter lookup logic

--- logicaldoc-core/src/main/java/com/logicaldoc/core/conversion/FormatConverterManager.java
+++ logicaldoc-core/src/main/java/com/logicaldoc/core/conversion/FormatConverterManager.java
@@ -448,7 +448,8 @@ public class FormatConverterManager {
 			log.warn("No format converter for file {}", inFileName);
 
 		// Get the first available and enabled converter
-		FormatConverter converter = formatConverters.stream().filter(c -> c.isEnabled()).findFirst().orElse(null);
+		FormatConverter converter = formatConverters.stream().filter(c -> c.isEnabled()).findFirst()
+				.orElse(new NotAvailableConverter());
 
 		// Check if a special binding is configured and points to an enabled
 		// converter
@@ -457,7 +458,7 @@ public class FormatConverterManager {
 		if (StringUtils.isNotEmpty(currentConverter) && formatConverters.size() > 1)
 			converter = formatConverters.stream()
 					.filter(conv -> conv.getClass().getName().equals(currentConverter) && conv.isEnabled()).findFirst()
-					.orElse(null);
+					.orElse(converter);
 
 		if (converter != null)
 			converter.loadParameters();
