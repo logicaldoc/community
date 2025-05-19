5640712933e24ae68a444e4754e3d9982c81d3a1
car031 <m.meschieri@logicaldoc.com>
2025-05-19 16:28:35 +0200

    Fixed enabled output formats lookup

--- logicaldoc-webapp/src/main/java/com/logicaldoc/web/data/FormatConvertersDataServlet.java
+++ logicaldoc-webapp/src/main/java/com/logicaldoc/web/data/FormatConvertersDataServlet.java
@@ -123,8 +123,8 @@ public class FormatConvertersDataServlet extends AbstractDataServlet {
 	private void writeConverter(PrintWriter writer, String inExt, String outExt, FormatConverter converter,
 			boolean parameters) {
 		ContextProperties conf = Context.get().getProperties();
-
-		if (converter.getClass().equals(NotAvailableConverter.class))
+		
+		if (converter==null || converter.getClass().equals(NotAvailableConverter.class))
 			return;
 
 		writer.print("<converter>");
