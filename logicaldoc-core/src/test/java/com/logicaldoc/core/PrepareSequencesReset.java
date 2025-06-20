4d114cd2ff8775257d607b4e49353b1cca8ed6c0
car031 <m.meschieri@logicaldoc.com>
2025-06-20 08:06:17 +0200

    Removed any reference to ld_hilo

--- logicaldoc-core/src/test/java/com/logicaldoc/core/PrepareSequencesReset.java
+++ logicaldoc-core/src/test/java/com/logicaldoc/core/PrepareSequencesReset.java
@@ -16,7 +16,11 @@ public class PrepareSequencesReset {
 			"ld_messagetemplate", "ld_contact", "ld_tenant", "ld_sequence", "ld_extoption", "ld_session", "ld_dashlet",
 			"ld_device", "ld_password_history", "ld_search", "ld_apikey", "ld_bookmark", "ld_webservicecall",
 			"ld_aimodel", "ld_aimodel_history", "ld_sampler", "ld_audit", "ld_barcodetemplate", "ld_event",
-			"ld_event_ref", "ld_chatmessage", "ld_emailaccount", "ld_automation_routine", "ld_automation_trigger");
+			"ld_event_ref", "ld_chatmessage", "ld_emailaccount", "ld_automation_routine", "ld_automation_trigger",
+			"ld_branding", "ld_readingrequest", "ld_form", "ld_archive", "ld_importfolder", "ld_importfolder_history",
+			"ld_ldapserver", "ld_ocr_history", "ld_report", "ld_retentionpolicy", "ld_robot", "ld_robot_history",
+			"ld_keystore", "ld_stamp", "ld_syndication", "ld_workflowtemplate", "ld_workflowhistory",
+			"ld_workflowtrigger","ld_ocrtemplate");
 
 	public static void main(String[] args) {
 		resetMariaDB();
