1872be78f7b3503162182111cef88b83d94712a6
car031 <m.meschieri@logicaldoc.com>
2025-08-22 15:05:37 +0200

    Make ContextInitializer tenant aware

--- logicaldoc-core/src/main/java/com/logicaldoc/core/ContextInitializer.java
+++ logicaldoc-core/src/main/java/com/logicaldoc/core/ContextInitializer.java
@@ -5,8 +5,6 @@ import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
-import jakarta.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -20,10 +18,12 @@ import com.logicaldoc.core.document.DocumentManager;
 import com.logicaldoc.core.folder.Folder;
 import com.logicaldoc.core.folder.FolderDAO;
 import com.logicaldoc.core.folder.FolderHistory;
-import com.logicaldoc.core.security.Tenant;
+import com.logicaldoc.core.security.TenantDAO;
 import com.logicaldoc.util.config.ContextProperties;
 import com.logicaldoc.util.io.FileUtil;
 import com.logicaldoc.util.io.ResourceUtil;
+
+import jakarta.annotation.Resource;
 
 /**
  * A super class for those initializers that prepare the environment for
@@ -48,14 +48,19 @@ public abstract class ContextInitializer implements ApplicationListener<ContextR
 	@Resource(name = "folderDAO")
 	protected FolderDAO folderDAO;
 
+	@Resource(name = "tenantDAO")
+	protected TenantDAO tenantDAO;
+
 	/**
 	 * Concrete implementations must override this method to put their
 	 * initialization logic.
 	 * 
+	 * @param tenantId Identifier of the tenant to initialize
+	 * 
 	 * @throws IOException Generic I/O error
 	 * @throws SQLException Error in the data layer
 	 */
-	protected abstract void initialize() throws IOException, SQLException;
+	protected abstract void initialize(long tenantId) throws IOException, SQLException;
 
 	@Override
 	public final void onApplicationEvent(ContextRefreshedEvent event) {
@@ -63,7 +68,8 @@ public abstract class ContextInitializer implements ApplicationListener<ContextR
 			return;
 
 		try {
-			initialize();
+			for (Long tenantId : tenantDAO.findAllIds())
+				initialize(tenantId);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
@@ -81,7 +87,8 @@ public abstract class ContextInitializer implements ApplicationListener<ContextR
 	 * Creates a folder
 	 * 
 	 * @param path The path
-	 * @param transaction The current transaction
+	 * @param transaction The current transaction, it is mandatory and used to
+	 *        pick the tenant's ID
 	 * 
 	 * @return The created folder
 	 * 
@@ -98,14 +105,16 @@ public abstract class ContextInitializer implements ApplicationListener<ContextR
 		FolderHistory fHist = new FolderHistory();
 		BeanUtils.copyProperties(transaction, fHist, "event");
 		fHist.setId(0);
+		fHist.setTenantId(transaction.getTenantId());
 		Folder target = prepareFolder(FileUtil.getPath(documentPath), fHist);
 		String fileName = FileUtil.getName(documentPath);
 		Document doc = documentDAO
-				.findByFileNameAndParentFolderId(target.getId(), fileName, null, Tenant.DEFAULT_ID, null).stream()
-				.findFirst().orElse(null);
+				.findByFileNameAndParentFolderId(target.getId(), fileName, null, transaction.getTenantId(), null)
+				.stream().findFirst().orElse(null);
 		if (doc == null)
 			try (InputStream is = ResourceUtil.getInputStream(resourcePath)) {
 				Document trainingDoc = new Document();
+				trainingDoc.setTenantId(transaction.getTenantId());
 				trainingDoc.setFileName(fileName);
 				trainingDoc.setFolder(target);
 				trainingDoc.setLanguage("en");
