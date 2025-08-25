421385f0278686849051604e3fb59e8a535e4078
car031 <m.meschieri@logicaldoc.com>
2025-08-25 08:32:04 +0200

    some workbench updates

--- logicaldoc-core/src/test/java/com/logicaldoc/core/SequencesWorkbench.java
+++ logicaldoc-core/src/test/java/com/logicaldoc/core/SequencesWorkbench.java
@@ -1,6 +1,7 @@
 package com.logicaldoc.core;
 
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Just a development utility to produce the scripts to reset the sequences
@@ -10,26 +11,89 @@ import java.util.List;
  *
  */
 public class SequencesWorkbench {
-	private final static List<String> tables = List.of("ld_bookmark", "ld_document", "ld_generic", "ld_group",
+	private final static Set<String> tables = Set.of("ld_bookmark", "ld_document", "ld_generic", "ld_group",
 			"ld_history", "ld_link", "ld_menu", "ld_systemmessage", "ld_template", "ld_attributeset", "ld_ticket",
 			"ld_user", "ld_user_history", "ld_version", "ld_folder", "ld_folder_history", "ld_rating", "ld_note",
 			"ld_messagetemplate", "ld_contact", "ld_tenant", "ld_sequence", "ld_extoption", "ld_session", "ld_dashlet",
-			"ld_device", "ld_password_history", "ld_search", "ld_apikey", "ld_bookmark", "ld_webservicecall",
-			"ld_aimodel", "ld_aimodel_history", "ld_sampler", "ld_audit", "ld_barcodetemplate", "ld_event",
-			"ld_event_ref", "ld_chatmessage", "ld_emailaccount", "ld_automation_routine", "ld_automation_trigger",
-			"ld_branding", "ld_readingrequest", "ld_form", "ld_archive", "ld_importfolder", "ld_importfolder_history",
-			"ld_ldapserver", "ld_ocr_history", "ld_report", "ld_retentionpolicy", "ld_robot", "ld_robot_history",
-			"ld_keystore", "ld_stamp", "ld_syndication", "ld_workflowtemplate", "ld_workflowhistory",
-			"ld_workflowtrigger", "ld_ocrtemplate");
+			"ld_device", "ld_password_history", "ld_search", "ld_apikey", "ld_webservicecall", "ld_aimodel",
+			"ld_aimodel_history", "ld_sampler", "ld_audit", "ld_barcodetemplate", "ld_event", "ld_event_ref",
+			"ld_chatmessage", "ld_emailaccount", "ld_automation_routine", "ld_automation_trigger", "ld_branding",
+			"ld_readingrequest", "ld_form", "ld_archive", "ld_importfolder", "ld_importfolder_history", "ld_ldapserver",
+			"ld_ocr_history", "ld_report", "ld_retentionpolicy", "ld_robot", "ld_robot_history", "ld_keystore",
+			"ld_stamp", "ld_syndication", "ld_workflowtemplate", "ld_workflowhistory", "ld_workflowtrigger",
+			"ld_ocrtemplate");
 
 	public static void main(String[] args) {
+		System.out.println("total tables: " + tables.size());
+		System.out.println(tables.stream().collect(Collectors.joining(",")));
+
 		// resetMariaDB();
-		// resetMySQL();
+		resetMySQL();
 		// resetHSQLDB();
 		// resetSqlServer();
 		// resetOracle();
+		// resetPostreSQL();
 
-		resetPostreSQL();
+//		createMariaDB();
+//		createHSQLDB();
+//		createSqlServer();
+//		createOracle();
+//		createPostreSQL();
+	}
+
+	private static void createPostreSQL() {
+		System.out.println("\n\n--------------\nPostreSQL:\n");
+
+		for (String table : tables) {
+			System.out
+					.println("create sequence " + table + "_SEQ  increment by 50 start with START_" + table + "_SEQ;");
+		}
+
+		System.out.println("\n--------------\n");
+	}
+
+	private static void createOracle() {
+		System.out.println("\n\n--------------\nOracle:\n");
+
+		for (String table : tables) {
+			System.out
+					.println("create sequence " + table + "_SEQ  start with START_" + table + "_SEQ increment by 50;");
+		}
+
+		System.out.println("\n--------------\n");
+	}
+
+	private static void createSqlServer() {
+		System.out.println("\n\n--------------\nSQL Server:\n");
+
+		for (String table : tables) {
+			System.out
+					.println("create sequence " + table + "_SEQ  start with START_" + table + "_SEQ increment by 50;");
+		}
+
+		System.out.println("\n--------------\n");
+	}
+
+	private static void createHSQLDB() {
+		System.out.println("\n\n--------------\nMariaDB:\n");
+
+		for (String table : tables) {
+			System.out
+					.println("create sequence " + table + "_SEQ  start with START_" + table + "_SEQ increment by 50;");
+		}
+
+		System.out.println("\n--------------\n");
+	}
+
+	private static void createMariaDB() {
+		System.out.println("\n\n--------------\nMariaDB:\n");
+
+		for (String table : tables) {
+			System.out
+					.println("create sequence " + table + "_SEQ  increment by 50 start with START_" + table + "_SEQ;");
+		}
+
+		System.out.println("\n--------------\n");
 	}
 
 	private static void resetPostreSQL() {
@@ -57,11 +121,11 @@ $$""");
 		System.out.println("\n\n--------------\nOracle:\n");
 
 		for (String table : tables) {
-			System.out.println("declare rst number(19,0);");
+			System.out.println("declare\n  rst number(19,0);");
 			System.out.println("begin");
 			System.out.println("  select nvl(max(ld_id),0) + 50  into rst from " + table + ";");
 			System.out.println("  alter sequence " + table + "_SEQ RESTART START WITH rst;");
-			System.out.println("end");
+			System.out.println("end;");
 			System.out.println("");
 		}
 
@@ -112,7 +176,7 @@ $$""");
 			System.out.println("delete from " + table + "_SEQ;");
 		System.out.println(" ");
 		for (String table : tables)
-			System.out.println("insert into " + table + "_SEQ(next_val) select max(ld_id) + 50 from " + table + ";");
+			System.out.println("insert into " + table + "_SEQ select max(next_val) + 50 from ld_hilo where ld_sequence= '" + table + "';");
 
 		System.out.println("\n--------------\n");
 	}
