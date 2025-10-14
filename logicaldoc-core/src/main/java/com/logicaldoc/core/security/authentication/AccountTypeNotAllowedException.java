bdd8cce1f7fd522b2f8899fca42bbf71d2a5c28f
car031 <m.meschieri@logicaldoc.com>
2025-10-14 17:23:33 +0200

    javadoc fixes

--- logicaldoc-core/src/main/java/com/logicaldoc/core/security/authentication/AccountTypeNotAllowedException.java
+++ logicaldoc-core/src/main/java/com/logicaldoc/core/security/authentication/AccountTypeNotAllowedException.java
@@ -1,7 +1,7 @@
 package com.logicaldoc.core.security.authentication;
 
 /**
- * Raised when truing to enter with an account of a unknown type
+ * Raised when trying to enter with an account of a unknown type
  * 
  * @author Marco Meschieri - LogicalDOC
  * @since 8.8.4
