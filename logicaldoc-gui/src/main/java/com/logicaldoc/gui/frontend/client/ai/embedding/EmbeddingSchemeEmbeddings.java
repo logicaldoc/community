1767310863dd5b8a34910e2fedf0edd59cb4fed8
car031 <m.meschieri@logicaldoc.com>
2025-12-01 08:59:59 +0100

    completed deletions of embeddings

--- logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/ai/embedding/EmbeddingSchemeEmbeddings.java
+++ logicaldoc-gui/src/main/java/com/logicaldoc/gui/frontend/client/ai/embedding/EmbeddingSchemeEmbeddings.java
@@ -1,23 +1,37 @@
 package com.logicaldoc.gui.frontend.client.ai.embedding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.logicaldoc.gui.common.client.DefaultAsyncCallback;
+import com.logicaldoc.gui.common.client.beans.GUIAccessControlEntry;
+import com.logicaldoc.gui.common.client.beans.GUIDocument;
 import com.logicaldoc.gui.common.client.grid.ColoredListGridField;
 import com.logicaldoc.gui.common.client.grid.DateListGridField;
 import com.logicaldoc.gui.common.client.grid.FileNameListGridField;
 import com.logicaldoc.gui.common.client.grid.FileSizeListGridField;
-import com.logicaldoc.gui.common.client.grid.IndexedListGridField;
+import com.logicaldoc.gui.common.client.grid.FileVersionListGridField;
 import com.logicaldoc.gui.common.client.grid.RefreshableListGrid;
-import com.logicaldoc.gui.common.client.grid.VersionListGridField;
+import com.logicaldoc.gui.common.client.grid.TypeIconGridField;
 import com.logicaldoc.gui.common.client.i18n.I18N;
+import com.logicaldoc.gui.common.client.preview.PreviewPopup;
+import com.logicaldoc.gui.common.client.util.DocUtil;
 import com.logicaldoc.gui.common.client.util.ItemFactory;
+import com.logicaldoc.gui.common.client.util.LD;
 import com.logicaldoc.gui.common.client.widgets.InfoPanel;
-import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.types.ListGridFieldType;
+import com.logicaldoc.gui.frontend.client.ai.AIService;
+import com.logicaldoc.gui.frontend.client.document.DocumentsPanel;
+import com.logicaldoc.gui.frontend.client.services.DocumentService;
+import com.smartgwt.client.types.AutoFitWidthApproach;
 import com.smartgwt.client.types.SelectionStyle;
 import com.smartgwt.client.widgets.form.fields.SpinnerItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.menu.Menu;
+import com.smartgwt.client.widgets.menu.MenuItem;
+import com.smartgwt.client.widgets.menu.MenuItemSeparator;
 import com.smartgwt.client.widgets.toolbar.ToolStrip;
 import com.smartgwt.client.widgets.toolbar.ToolStripButton;
 
@@ -59,55 +73,33 @@ public class EmbeddingSchemeEmbeddings extends EmbeddingSchemeDetailsTab {
 		// Prepare a panel containing a title and the documents number
 		final InfoPanel infoPanel = new InfoPanel("");
 
-		ListGridField id = new ColoredListGridField("id");
-		id.setWidth(50);
-		id.setHidden(true);
+		ListGridField docId = new ColoredListGridField("docid");
+		docId.setWidth(50);
+		docId.setHidden(true);
 
 		ListGridField size = new FileSizeListGridField("size", I18N.message("size"));
-		size.setAlign(Alignment.CENTER);
-		size.setType(ListGridFieldType.FLOAT);
 
-		ListGridField version = new VersionListGridField();
-		version.setAlign(Alignment.CENTER);
+		ListGridField fileVersion = new FileVersionListGridField();
+		fileVersion.setAutoFitWidth(true);
+		fileVersion.setAutoFitWidthApproach(AutoFitWidthApproach.BOTH);
 
-		ListGridField lastModified = new DateListGridField("lastModified", "lastmodified");
-
-		ListGridField publisher = new ColoredListGridField("publisher", I18N.message("publisher"), 90);
-		publisher.setAlign(Alignment.CENTER);
-
-		ListGridField published = new DateListGridField("published", "publishedon");
-
-		ListGridField creator = new ColoredListGridField("creator", I18N.message("creator"), 90);
-		creator.setAlign(Alignment.CENTER);
-
-		ListGridField created = new DateListGridField("created", "createdon");
-
-		ListGridField customId = new ColoredListGridField("customId", I18N.message("customid"), 110);
-
-		ListGridField indexable = new IndexedListGridField();
+		ListGridField published = new DateListGridField("date", "publishedon");
 
 		ListGridField filename = new FileNameListGridField();
 
-		ListGridField lockUserId = new ColoredListGridField("lockUserId", " ", 24);
-		lockUserId.setHidden(true);
+		ListGridField icon = new TypeIconGridField();
 
-		embeddings = new RefreshableListGrid() {
-			@Override
-			protected String getCellCSSText(ListGridRecord rec, int rowNum, int colNum) {
-				if (rec == null)
-					return "";
-				if (getFieldName(colNum).equals("filename")) {
-					if ("stop".equals(rec.getAttribute("immutable"))) {
-						return "color: #888888; font-style: italic;";
-					} else {
-						return super.getCellCSSText(rec, rowNum, colNum);
-					}
-				} else {
-					return super.getCellCSSText(rec, rowNum, colNum);
-				}
-			}
-		};
+		embeddings = new RefreshableListGrid();
 		embeddings.setEmptyMessage(I18N.message("notitemstoshow"));
+		embeddings.setShowRecordComponents(true);
+		embeddings.setShowRecordComponentsByCell(true);
+		embeddings.setCanFreezeFields(true);
+		embeddings.setAutoFetchData(true);
+		embeddings.setSelectionType(SelectionStyle.MULTIPLE);
+		embeddings.setShowFilterEditor(true);
+		embeddings.setFilterOnKeypress(true);
+		embeddings.setDataSource(new EmbeddingsDS(embeddingScheme.getId(), 100));
+		embeddings.setFields(docId, icon, filename, size, fileVersion, published);
 
 		embeddings.addCellContextClickHandler(event -> {
 			showContextMenu();
@@ -117,17 +109,6 @@ public class EmbeddingSchemeEmbeddings extends EmbeddingSchemeDetailsTab {
 		embeddings.addDataArrivedHandler(dataArrivedEvent -> infoPanel
 				.setMessage(I18N.message("shownembeddings", Integer.toString(embeddings.getTotalRows()))));
 
-		embeddings.setShowRecordComponents(true);
-		embeddings.setShowRecordComponentsByCell(true);
-		embeddings.setCanFreezeFields(true);
-		embeddings.setAutoFetchData(true);
-		embeddings.setSelectionType(SelectionStyle.MULTIPLE);
-		embeddings.setShowFilterEditor(true);
-		embeddings.setFilterOnKeypress(true);
-		embeddings.setDataSource(new EmbeddingsDS(embeddingScheme.getId(), 100));
-		embeddings.setFields(id, indexable, filename, size, lastModified, version, publisher, published, creator,
-				created, customId);
-
 		VLayout contents = new VLayout();
 		contents.setMembers(toolStrip, infoPanel, embeddings);
 
@@ -135,8 +116,64 @@ public class EmbeddingSchemeEmbeddings extends EmbeddingSchemeDetailsTab {
 	}
 
 	private void showContextMenu() {
-		// TODO Auto-generated method stub
+		final ListGridRecord[] selection = embeddings.getSelectedRecords();
+		List<Long> docIds = new ArrayList<>();
+		for (ListGridRecord rec : selection)
+			docIds.add(rec.getAttributeAsLong("docid"));
 
+		DocumentService.Instance.get().getAllowedPermissions(docIds, new DefaultAsyncCallback<>() {
+			@Override
+			public void handleSuccess(GUIAccessControlEntry enabledPermissions) {
+				Menu contextMenu = new Menu();
+				Long selectedEmbeddingDocId = selection[0].getAttributeAsLong("docid");
+				Long selectedEmbeddingFolderId = selection[0].getAttributeAsLong("folderid");
+
+				MenuItem preview = new MenuItem();
+				preview.setTitle(I18N.message("preview"));
+				preview.setEnabled(enabledPermissions.isPreview());
+				preview.addClickHandler(event -> DocumentService.Instance.get().getById(selectedEmbeddingDocId,
+						new DefaultAsyncCallback<>() {
+							@Override
+							public void handleSuccess(GUIDocument doc) {
+								new PreviewPopup(doc).show();
+							}
+						}));
+
+				MenuItem open = new MenuItem();
+				open.setTitle(I18N.message("openinfolder"));
+				open.setEnabled(enabledPermissions.isRead());
+				open.addClickHandler(event -> {
+					destroy();
+
+					if (com.logicaldoc.gui.common.client.Menu.enabled(com.logicaldoc.gui.common.client.Menu.DOCUMENTS))
+						DocumentsPanel.get().openInFolder(selectedEmbeddingFolderId, selectedEmbeddingDocId);
+				});
+
+				MenuItem download = new MenuItem();
+				download.setTitle(I18N.message("download"));
+				download.setEnabled(enabledPermissions.isDownload());
+				download.addClickHandler(event -> DocUtil.download(selectedEmbeddingDocId, null));
+
+				MenuItem delete = new MenuItem();
+				delete.setTitle(I18N.message("ddelete"));
+				delete.addClickHandler(
+						event -> LD.ask(I18N.message("question"), I18N.message("confirmdelete"), confirm -> {
+							if (Boolean.TRUE.equals(confirm)) {
+								AIService.Instance.get().removeEmbeddings(embeddingScheme.getId(), docIds,
+										new DefaultAsyncCallback<>() {
+											@Override
+											public void handleSuccess(Void result) {
+												embeddings.removeSelectedData();
+												embeddings.deselectAllRecords();
+											}
+										});
+							}
+						}));
+
+				contextMenu.setItems(preview, download, open, new MenuItemSeparator(), delete);
+				contextMenu.showContextMenu();
+			}
+		});
 	}
 
 	private void refresh(Integer max) {
