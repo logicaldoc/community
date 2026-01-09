d9d9b60bfbcfe16bf01cc4d07f7b0a2b1b3e950f
car031 <m.meschieri@logicaldoc.com>
2026-01-09 15:22:30 +0100

    Updated release to 9.2.3

--- logicaldoc-core/src/main/java/com/logicaldoc/core/SystemInfo.java
+++ logicaldoc-core/src/main/java/com/logicaldoc/core/SystemInfo.java
@@ -32,7 +32,7 @@ public class SystemInfo {
 
 	protected String product = "LogicalDOC";
 
-	protected String release = "9.2.2";
+	protected String release = "9.2.3";
 
 	protected String year = "2006-2026";
 
--- logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
+++ logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/beans/GUIInfo.java
@@ -18,7 +18,7 @@ public class GUIInfo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String release = "9.2.2";
+	private String release = "9.2.3";
 
 	private String year = "2006-2026";
 
