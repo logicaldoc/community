7ae7a8194bda0ec14816cf51113ddcf0ff08e729
car031 <m.meschieri@logicaldoc.com>
2026-09-01 10:39:12 +0200

    more flexible HTML Editor

--- logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/automation/HtmlItemEditor.java
+++ logicaldoc-gui/src/main/java/com/logicaldoc/gui/common/client/automation/HtmlItemEditor.java
@@ -5,6 +5,7 @@ import com.logicaldoc.gui.common.client.util.Util;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.ContentsType;
 import com.smartgwt.client.types.HeaderControls;
+import com.smartgwt.client.widgets.HTMLFlow;
 import com.smartgwt.client.widgets.HTMLPane;
 import com.smartgwt.client.widgets.HeaderControl;
 import com.smartgwt.client.widgets.Window;
@@ -22,98 +23,119 @@ import com.smartgwt.client.widgets.toolbar.ToolStripButton;
  */
 public class HtmlItemEditor extends Window {
 
-	private VLayout layout = null;
+    private VLayout layout = null;
 
-	private FormItem item;
+    private FormItem item;
 
-	private ChangedHandler handler;
+    private HTMLFlow flow;
 
-	public HtmlItemEditor(FormItem item, ChangedHandler handler) {
-		this.item = item;
-		this.handler = handler;
+    private ChangedHandler handler;
 
-		HeaderControl closeIcon = new HeaderControl(HeaderControl.CLOSE, event -> onClose());
+    public HtmlItemEditor(HTMLFlow flow, ChangedHandler handler) {
+        this.flow = flow;
+        this.handler = handler;
 
-		setHeaderControls(HeaderControls.HEADER_LABEL, closeIcon);
-		setTitle(item.getTitle());
+    }
 
-		setWidth(com.google.gwt.user.client.Window.getClientWidth());
-		setHeight(com.google.gwt.user.client.Window.getClientHeight());
-		setCanDragResize(true);
-		setIsModal(true);
-		setShowModalMask(true);
-		centerInPage();
+    public HtmlItemEditor(FormItem item, ChangedHandler handler) {
+        this.item = item;
+        this.handler = handler;
+        init();
+    }
 
-		addCloseClickHandler(event -> onClose());
+    private void init() {
+        HeaderControl closeIcon = new HeaderControl(HeaderControl.CLOSE, event -> onClose());
 
-		layout = new VLayout();
-		layout.setWidth100();
+        setHeaderControls(HeaderControls.HEADER_LABEL, closeIcon);
+        setTitle(item != null ? item.getTitle() : I18N.message("htmleditor"));
 
-		prepareBody();
+        setWidth(com.google.gwt.user.client.Window.getClientWidth());
+        setHeight(com.google.gwt.user.client.Window.getClientHeight());
+        setCanDragResize(true);
+        setIsModal(true);
+        setShowModalMask(true);
+        centerInPage();
 
-		addItem(layout);
-	}
+        addCloseClickHandler(event -> onClose());
 
-	/**
-	 * Prepares the popup body
-	 */
-	private void prepareBody() {
-		setSavedHtmlContent(item.getValue() != null ? item.getValue().toString() : "");
+        layout = new VLayout();
+        layout.setWidth100();
 
-		HTMLPane editorPanel = new HTMLPane();
-		editorPanel.setID("htmlEditorPanel");
-		editorPanel.setShowEdges(false);
-		editorPanel.setContentsURL(Util.webEditorUrl(getHeight() - 230));
-		editorPanel.setContentsType(ContentsType.PAGE);
+        prepareBody();
 
-		layout.addMember(editorPanel);
+        addItem(layout);
+    }
 
-		ToolStrip toolStrip = new ToolStrip();
-		toolStrip.setWidth100();
-		toolStrip.setAlign(Alignment.RIGHT);
-		toolStrip.addFill();
+    private String getContent() {
+        if (item != null)
+            return item.getValue() != null ? item.getValue().toString() : "";
+        else
+            return flow.getContents();
+    }
 
-		ToolStripButton close = new ToolStripButton();
-		close.setTitle(I18N.message("close"));
-		toolStrip.addButton(close);
-		close.addClickHandler(event -> onClose());
+    /**
+     * Prepares the popup body
+     */
+    private void prepareBody() {
+        setSavedHtmlContent(getContent());
 
-		layout.setMembers(toolStrip, editorPanel);
-	}
+        HTMLPane editorPanel = new HTMLPane();
+        editorPanel.setID("htmlEditorPanel");
+        editorPanel.setShowEdges(false);
+        editorPanel.setContentsURL(Util.webEditorUrl(getHeight() - 230));
+        editorPanel.setContentsType(ContentsType.PAGE);
 
-	private void onClose() {
-		String editedHtml = getSavedHtmlContent() != null ? getSavedHtmlContent() : "";
-		String originalHtml = item.getValue() != null ? item.getValue().toString() : "";
-		if (!editedHtml.equals(originalHtml)) {
-			item.setValue(editedHtml);
-			if (handler != null)
-				handler.onChanged(null);
-		}
-		setSavedHtmlContent("");
-		destroy();
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		return super.equals(other);
-	}
+        layout.addMember(editorPanel);
 
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
-	
-	/**
-	 * Saves the actual item content in a global Javascript variable that can be
-	 * accessed by the HTML editor
-	 * 
-	 * @param value the inputed value to save
-	 */
-	public static native void setSavedHtmlContent(String value) /*-{
+        ToolStrip toolStrip = new ToolStrip();
+        toolStrip.setWidth100();
+        toolStrip.setAlign(Alignment.RIGHT);
+        toolStrip.addFill();
+
+        ToolStripButton close = new ToolStripButton();
+        close.setTitle(I18N.message("close"));
+        toolStrip.addButton(close);
+        close.addClickHandler(event -> onClose());
+
+        layout.setMembers(toolStrip, editorPanel);
+    }
+
+    private void onClose() {
+        String editedHtml = getSavedHtmlContent() != null ? getSavedHtmlContent() : "";
+        String originalHtml = item.getValue() != null ? item.getValue().toString() : "";
+        if (!editedHtml.equals(originalHtml)) {
+            if (item != null)
+                item.setValue(editedHtml);
+            else
+                flow.setContents(editedHtml);
+            if (handler != null)
+                handler.onChanged(null);
+        }
+        setSavedHtmlContent("");
+        destroy();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    /**
+     * Saves the actual item content in a global Javascript variable that can be
+     * accessed by the HTML editor
+     * 
+     * @param value the inputed value to save
+     */
+    public static native void setSavedHtmlContent(String value) /*-{
 		$wnd.savedHtmlContent = value;
 	}-*/;
 
-	public static native String getSavedHtmlContent() /*-{
+    public static native String getSavedHtmlContent() /*-{
 		return $wnd.savedHtmlContent;
 	}-*/;
 }
